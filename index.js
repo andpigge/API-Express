95612bdf714d604b84b41d944e1ca885bd0efdb5
Rustam <rustamaaaa@bk.ru>
2024-03-20 18:11:40 +0300

    added get and post routes

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-import http from 'http';
+import express from 'express';
 
 const SETTINGS = {
+  host: 'http://localhost',
   port: 8000,
-  host: '127.0.0.1',
 };
 
-const server = http.createServer((req, res) => {
-  switch (req.method) {
-    case 'GET':
-      res.getHeader('Content-Type', 'text/plain;');
-      switch (req.url) {
-        case '/hello':
-          res.statusCode = 200;
-          res.end('Hello world!');
-          break;
-        default:
-          res.statusCode = 404;
-          res.end('404 not found');
-      }
-    }
+const app = express();
+
+app.all('/*', (req, res, next) => {
+  console.log('All');
+  next();
 });
 
-server.listen(
-  SETTINGS,
-  () => console.log(
-    `Сервер успешно запущен на ${SETTINGS.host}:${SETTINGS.port} порте!`
-  )
-);
+const cb = (req, res, next) => {
+  console.log('CB');
+  next();
+}
+
+app.get('/hello', cb, (req, res) => {
+  res.send('Привет!');
+})
+
+app.post('/hello', (req, res) => {
+  res.send('Привет!');
+});
+
+app.listen(SETTINGS.port, () => console.log(
+  `Сервер успешно запущен на ${SETTINGS.host}:${SETTINGS.port} порте!`
+));
